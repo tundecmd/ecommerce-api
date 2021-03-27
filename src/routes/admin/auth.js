a19e3d36b8b1c1de5e4d361adce880f2e013505a
Adepoju Lateef <ademustexcel@gmail.com>
2021-03-27 07:10:05 +0100

    removed requireSignin from signout route

--- src/routes/admin/auth.js
+++ src/routes/admin/auth.js
@@ -9,7 +9,7 @@ router.post('/admin/signin', validateSigninRequest, isRequestValidated, signin);
 
 router.post('/admin/signup', validateSignupRequest, isRequestValidated, signup);
 
-router.post('/admin/signout', requireSignin, signout)
+router.post('/admin/signout', signout);
 
 // router.post('/profile', requireSignin,                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 (req, res) => {
 //     res.status(200).json({ user: 'profile' })
