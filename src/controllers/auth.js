79fa585240afd153e3e9d3cb827966d64af38ba5
Adepoju Lateef <ademustexcel@gmail.com>
2021-04-25 13:00:48 +0100

    changed username from using Math.random function to using shortid library

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -2,6 +2,7 @@ const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 const { validationResult }= require('express-validator');
 const bcrypt =  require("bcrypt");
+const shortid = require('shortid');
 
 
 exports.signup = (req, res) => {
@@ -26,7 +27,7 @@ exports.signup = (req, res) => {
             lastName,
             email,
             hash_password,
-            username: Math.random().toString()
+            username: shortid.generate()
         })
 
         _user.save((error, data) => {
